aba1ef711e408bfb65ad939945f96c04bd86a9f0
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-18 11:28:13 +0100

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -9,9 +9,8 @@ BEGIN
         JOIN corrections as C ON U.id=C.user_id 
         JOIN projects AS P ON C.project_id=P.id 
         GROUP BY U.id)
-    AS WA
-    SET U.average_score = WA.w_avg 
-    WHERE U.id=WA.id;
+    AS WA ON U.id=WA.id
+    SET U.average_score = WA.w_avg;
 END
 $$
 DELIMITER ;
