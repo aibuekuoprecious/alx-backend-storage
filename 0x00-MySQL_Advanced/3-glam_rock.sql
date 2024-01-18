fd98ff538da8f9355d64b91c9f0cb5ec215a2392
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-18 11:46:41 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,16 @@
--- script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+-- SQL script to list all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+    band_name,
+    IF(splitted.lifespan IS NULL, 0, splitted.lifespan) AS lifespan
+FROM
+    (
+        SELECT
+            band_name,
+            FLOOR((DATEDIFF(2022, formed) + 1) / 365) AS lifespan
+        FROM
+            metal_bands
+    ) AS splitted
+    RIGHT JOIN glam_rock_bands AS glam
+    ON splitted.band_name = glam.band_name
+ORDER BY
+    lifespan DESC, band_name;
