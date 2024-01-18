a51f5ea35e1ce2ceca91c8e34db660dd443150d9
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-18 11:41:32 +0100

    Update 6-bonus.sql

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -7,10 +7,10 @@ CREATE PROCEDURE AddBonus(
     IN score FLOAT)
 BEGIN
     DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-    THEN
-        INSERT INTO projects (name) VALUES (project_name);
-    END IF;
+
+    -- Use INSERT IGNORE to handle the case where the project already exists
+    INSERT IGNORE INTO projects (name) VALUES (project_name);
+
     SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
 END
