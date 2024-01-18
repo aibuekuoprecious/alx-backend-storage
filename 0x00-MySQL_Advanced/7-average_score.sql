0c526f220c6be9f3c2ce41e4c4c1ce5f1609d465
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-18 11:39:31 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,12 @@
--- script that creates a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student
+--  script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    
-    -- Use IFNULL to handle the case where there are no corrections for the user
-    SELECT IFNULL(AVG(score), 0) INTO avg_score FROM corrections WHERE user_id = user_id;
-    
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
 END
 $$
 DELIMITER ;
