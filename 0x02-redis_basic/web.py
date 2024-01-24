225864696ba2abec90aac0b677e8aa5a3c2b364c
Aibueku O. Precious <aibuekuprecious@gmail.com>
2024-01-24 15:35:03 +0100

    updated

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,20 +6,20 @@ import redis
 import requests
 from typing import Callable
 
-redis_client = redis.Redis()
+redis_ = redis.Redis()
 
 
-def count_requests(func: Callable) -> Callable:
-    """ Decorator for counting requests """
-    @wraps(func)
-    def wrapper(url):  
-        """ Wrapper function for counting requests """
-        redis_client.incr(f"count:{url}")
-        cached_html = redis_client.get(f"cached:{url}")
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
         if cached_html:
             return cached_html.decode('utf-8')
-        html = func(url)
-        redis_client.setex(f"cached:{url}", 10, html)
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
         return html
 
     return wrapper
@@ -27,6 +27,6 @@ def count_requests(func: Callable) -> Callable:
 
 @count_requests
 def get_page(url: str) -> str:
-    """ Retrieve the HTML content of a URL """
-    response = requests.get(url)
-    return response.text
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
